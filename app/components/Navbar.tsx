045e81707c4a70b1f2bdc887477d3cd9769bd2cb
Bruce Simiyu <bruce14simiyu@gmail.com>
2023-10-09 04:52:47 +0300

    commit one

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+  return (
+    <div>
+        <h1 className="ml-4 font-bold">Hello Bruce</h1>
+    </div>
+  )
+}
+
+export default Navbar
